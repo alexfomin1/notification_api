a8271d22adc971a4632d0e88edfe87b75fe43628
alex <me@fomin3.ru>
2022-09-22 12:04:03 +0300

    serializers added

--- notify_api/clients/views.py
+++ notify_api/clients/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Client
+from .serializers import ClientSerializer
+
 
 # Create your views here.
+class ClientsAPIView(generics.ListAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
