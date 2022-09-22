29624efaaf235e4d18eeada645f61e1c6f657441
alex <me@fomin3.ru>
2022-09-22 11:53:42 +0300

    admin updated

--- notify_api/clients/models.py
+++ notify_api/clients/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Client(models.Model):
-    client_id = models.IntegerField(unique=True)
+    client_id = models.CharField(max_length=255, unique=True)
     client_phone = models.IntegerField()
     client_op = models.IntegerField()
     client_tag = models.CharField(max_length=255)
