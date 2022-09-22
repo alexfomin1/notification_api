3f5858e6e86cb6a89d8cb75009cb0e4d1b785a52
alex <me@fomin3.ru>
2022-09-22 16:17:08 +0300

    Viewset integration

--- notify_api/clients/views.py
+++ notify_api/clients/views.py
@@ -1,23 +1,12 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Client
 from .serializers import ClientSerializer
 
-
-class ClientsAPIList(generics.ListCreateAPIView):
-    queryset = Client.objects.all()
-    serializer_class = ClientSerializer
-
-
-class ClientsAPIUpdate(generics.UpdateAPIView):
-    queryset = Client.objects.all()
-    serializer_class = ClientSerializer
-
-
-class ClientsAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ClientsViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
--- notify_api/notify_api/urls.py
+++ notify_api/notify_api/urls.py
@@ -13,16 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from clients.views import *
 from django.contrib import admin
 
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'clients', ClientsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/clientslist/', ClientsAPIList.as_view()),
-    path('api/v1/clientslist/<int:pk>/', ClientsAPIUpdate.as_view()),
-    path('api/v1/clientdetail/<int:pk>/', ClientsAPIDetailView.as_view())
+    path('api/v1/', include(router.urls)),
 ]
