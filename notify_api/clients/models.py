a58e1b79c7e114bff29fbdb0fdf634817212b639
alex <me@fomin3.ru>
2022-09-22 11:31:40 +0300

    clients app added

---
+++ notify_api/clients/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Client(models.Model):
+    client_id = models.IntegerField(unique=True)
+    client_phone = models.IntegerField()
+    client_op = models.IntegerField()
+    client_tag = models.CharField(max_length=255)
+    client_zone = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.client_id
--- notify_api/notify_api/settings.py
+++ notify_api/notify_api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'mailing.apps.MailingConfig',
+    'clients.apps.ClientsConfig',
 ]
 
 MIDDLEWARE = [
